3919fd5b301889e4af8a26c9519338c368a547db
Giancarlo Castañeda <castag.gian@gmail.com>
2024-06-12 19:23:23 -0500

    Initial Commit

---
+++ NarutoWiki/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NarutoWiki
+//
+//  Created by Giancarlo Castañeda Garcia on 12/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NarutoWiki/NarutoWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NarutoWikiApp.swift
+//  NarutoWiki
+//
+//  Created by Giancarlo Castañeda Garcia on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct NarutoWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
