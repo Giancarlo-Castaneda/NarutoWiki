dfacc6f20f85d5c1ed5f994f198c4c985a30d1a8
Giancarlo Castañeda <castag.gian@gmail.com>
2024-11-06 22:43:49 -0500

    chore(content-menu): add parallax header

--- NarutoWiki/Features/ContentMenu/ContentMenuView.swift
+++ NarutoWiki/Features/ContentMenu/ContentMenuView.swift
@@ -2,7 +2,8 @@ import ComposableArchitecture
 import SwiftUI
 
 struct ContentMenuView: View {
-    @Bindable var store : StoreOf<ContentMenuReducer>
+
+    @Bindable var store: StoreOf<ContentMenuReducer>
     let columns: Int = 2
 
     var body: some View {
@@ -10,38 +11,41 @@ struct ContentMenuView: View {
         let itemWidth = (screenWidth - CGFloat((columns + 1) * 10)) / CGFloat(columns)
 
         NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
-            VStack {
-                if let titleImage = store.titleImage {
-                    AsyncLazyImageView(url: titleImage)
-                        .frame(maxWidth: 300)
-                        .padding(.bottom)
+            ScrollView {
+                ParallaxHeader(coordinateSpace: CoordinateSpace.local, defaultHeight: 130) {
+                    if let titleImage = store.titleImage {
+                        AsyncLazyImageView(url: titleImage)
+                            .frame(maxWidth: 300)
+                            .padding(.bottom)
+                    }
                 }
-                ScrollView {
-                    LazyVGrid(
-                        columns: Array(repeating: GridItem(.fixed(itemWidth), spacing: 10), count: columns),
-                        spacing: 10
-                    ) {
-                        ForEach(store.contents, id: \.id) { item in
-                            NavigationLink(state: getDesiredDestination(id: item.id)) {
-                                VStack {
-                                    Text(item.title)
-                                        .foregroundStyle(Color.primary)
-                                        .padding(.top)
-                                        .padding(.horizontal, 10)
-                                    if let url = URL(string: item.image) {
-                                        AsyncLazyImageView(url: url)
-                                            .frame(width: itemWidth - 20, height: itemWidth * 0.6)
-                                            .padding(10)
-                                    }
+                LazyVGrid(
+                    columns: Array(repeating: GridItem(.fixed(itemWidth), spacing: 10), count: columns),
+                    spacing: 10
+                ) {
+                    ForEach(store.contents, id: \.id) { item in
+                        NavigationLink(state: getDesiredDestination(id: item.id)) {
+                            VStack {
+                                Text(item.title)
+                                    .foregroundStyle(Color.primary)
+                                    .padding(.top)
+                                    .padding(.horizontal, 10)
+                                if let url = URL(string: item.image) {
+                                    AsyncLazyImageView(url: url)
+                                        .frame(width: itemWidth - 20, height: itemWidth * 0.6)
+                                        .padding(10)
                                 }
-                                .roundedCornerFill(borderColor: Color.brown.opacity(0.3),
-                                                   radius: 10, corners: [.allCorners])
                             }
+                            .roundedCornerFill(borderColor: Color.brown.opacity(0.3),
+                                               radius: 10, corners: [.allCorners])
+                            .roundedCornerFill(borderColor: .white,
+                                               radius: 10, corners: [.allCorners])
                         }
                     }
                 }
-                .scrollIndicators(.hidden)
             }
+            .scrollIndicators(.hidden)
+            .coordinateSpace(name: CoordinateSpace.local)
         } destination: { store in
             switch store.case {
             case let .characterList(store):
@@ -63,28 +67,20 @@ struct ContentMenuView: View {
         switch id {
         case 1:
             return .characterList(CharacterListReducer.State(usage: .allCharacters))
-
         case 2:
             return .villageList(CharacterGroupListReducer.State(expectedUsage: .villages))
-
         case 3:
             return .villageList(CharacterGroupListReducer.State(expectedUsage: .clans))
-
         case 4:
             return .villageList(CharacterGroupListReducer.State(expectedUsage: .teams))
-
         case 5:
             return .villageList(CharacterGroupListReducer.State(expectedUsage: .kekkeiGenkais))
-
         case 6:
             return .characterList(CharacterListReducer.State(usage: .kara))
-
         case 7:
             return .characterList(CharacterListReducer.State(usage: .akatsuki))
-
         case 8:
             return .characterList(CharacterListReducer.State(usage: .tailedBeasts))
-
         default:
             return nil
         }
